d9155ec61f0fa8c5ae76bbf24a62e357aa722c3b
magefra9 <magefra9@hotmail.com>
2022-03-06 12:27:20 -0600

    Agregando OnModelCreating

--- RestauranteMango/Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ RestauranteMango/Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -13,7 +13,51 @@ namespace Mango.Services.ProductAPI.DbContexts
 
         public DbSet<Product> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name="Petacon",
+                Price = 14,
+                Description = "Mango presentativo de Veracruz",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Petacon",
+                Price = 10.99,
+                Description = "Mango presentativo de Veracruz",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
+
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Petacon",
+                Price = 14.222,
+                Description = "Mango presentativo de Veracruz",
+                ImageUrl = "",
+                CategoryName = "Dessert"
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Petacon",
+                Price = 18,
+                Description = "Mango presentativo de Veracruz",
+                ImageUrl = "",
+                CategoryName = "Entree"
+            });
+        }
 
     }
 }
