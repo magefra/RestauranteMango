d1091a533cc39a0e7f04b262c61d6f958a84382f
magefra9 <magefra9@hotmail.com>
2022-03-09 18:40:19 -0600

    Agregando AddIdentityServer

--- RestauranteMango/Mango.Services.Identity/Program.cs
+++ RestauranteMango/Mango.Services.Identity/Program.cs
@@ -1,7 +1,37 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+var identity = builder.Services.AddIdentityServer(opt =>
+{
+    opt.Events.RaiseErrorEvents = true;
+    opt.Events.RaiseInformationEvents = true;
+    opt.Events.RaiseFailureEvents = true;
+    opt.Events.RaiseSuccessEvents = true;
+    opt.EmitStaticAudienceClaim = true;
+})
+.AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+identity.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -17,6 +47,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
