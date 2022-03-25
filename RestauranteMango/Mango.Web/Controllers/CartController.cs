5793b63c4dcc11b3996b08674b7b395ea6303622
magefra9 <magefra9@hotmail.com>
2022-03-24 18:11:04 -0600

    Agregando CartController

---
+++ RestauranteMango/Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
