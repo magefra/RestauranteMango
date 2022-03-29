7e638309ce648ec97800ce98fd45b710653667fd
magefra9 <magefra9@hotmail.com>
2022-03-29 16:04:48 -0600

    Agregando AzureServiceBus

---
+++ RestauranteMango/Mango.messageBus/AzureServiceBus.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.messageBus
+{
+    public class AzureServiceBus : IBaseMessage
+    {
+        //Inprovisionado
+        private string connectionString = "Endpoint=sb://mangdorestaurant.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=G6IDn+LvlHqQXeh2i+cfjyAbEE6Jjh+EIDhYrupMy30=";
+
+
+        public async Task PublishMessage(BaseMessage message, string topicName)
+        {
+            ISenderClient senderClient = new TopicClient(connectionString, topicName);
+
+            var Jsonmessage = JsonConvert.SerializeObject(message);
+            var finalMessage = new Message(Encoding.UTF8.GetBytes(Jsonmessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+
+            await senderClient.SendAsync(finalMessage);
+
+
+            await senderClient.CloseAsync();
+        }
+    }
+}
