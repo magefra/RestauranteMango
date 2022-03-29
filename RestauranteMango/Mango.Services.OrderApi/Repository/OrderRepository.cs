9607f6d32e151ad6bcdecc4ab433cbf0b5b1a5a1
magefra9 <magefra9@hotmail.com>
2022-03-29 17:05:34 -0600

    Agregando OrderRepository

---
+++ RestauranteMango/Mango.Services.OrderApi/Repository/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Services.OrderApi.DbContexts;
+using Mango.Services.OrderApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderApi.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _dbContext;
+
+        public OrderRepository(DbContextOptions<ApplicationDbContext> dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            await using var _db = new ApplicationDbContext(_dbContext);
+            _db.OrderHeaders.Add(orderHeader);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            await using var _db = new ApplicationDbContext(_dbContext);
+            var orderHeaderFromDb = await _db.OrderHeaders.FirstOrDefaultAsync(u => u.OrderHeaderId == orderHeaderId);
+            if (orderHeaderFromDb != null)
+            {
+                orderHeaderFromDb.PaymentStatus = paid;
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
