813077b0dd06fe304de85305c95c07d344735716
magefra9 <magefra9@hotmail.com>
2022-04-11 18:37:05 -0500

    Agregando capturas de pantalla

--- RestauranteMango/Mango.Gateway.Solution/Program.cs
+++ RestauranteMango/Mango.Gateway.Solution/Program.cs
@@ -1,6 +1,42 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", opt =>
+    {
+        opt.Authority = "https://localhost:7173/";
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddOcelot();
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
+
+app.UseOcelot().Wait();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 
 app.Run();
